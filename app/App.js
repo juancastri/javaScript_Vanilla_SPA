ce42d688bc26c53f4f6375c06d8027e801f6d368
Your Name <you@example.com>
2023-10-21 18:16:23 -0500

    Estructura del proyecto

---
+++ app/App.js
@@ -0,0 +1,4 @@
+//Componente padre 
+export function App() {
+    document.getElementById("root").innerHTML = '<h1>Bienvenidos a mi primer SPA</h1>';
+}
---
+++ app/index.js
@@ -0,0 +1,6 @@
+//Genera la invocación de todos los archivos
+import { App } from "./App.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", App);
